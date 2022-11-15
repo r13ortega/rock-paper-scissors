793b36b54406e1017aaead02baa92c616ee6dee8
r1ort <reynaldoortega2013@gmail.com>
2022-11-15 11:43:03 -0600

    created rock paper scissors spock lizard

---
+++ src/Main.java
@@ -0,0 +1,77 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    //this creates an ArrayList of Strings
+    public static ArrayList<String> moves = new ArrayList<>();
+
+    public static void main(String[] args) {
+        //this adds Strings to the ArrayList by using the method moveSetter(), MUST BE FIRST TO HAVE REQUIRED MOVESET
+        moveSetter();
+        movesPrinter();
+        Scanner scan = new Scanner(System.in);
+        String playerMove = scan.nextLine();
+        String cpuMove = null;
+        Random rando = new Random();
+        int ranMove = rando.nextInt(4);
+        if (ranMove == 0){
+            cpuMove = "rock";
+        } else if (ranMove == 1) {
+            cpuMove = "paper";
+        }else if (ranMove == 2) {
+            cpuMove = "scissors";
+        }else if (ranMove == 3) {
+            cpuMove = "lizard";
+        }else if (ranMove == 4) {
+            cpuMove = "spock";
+        }
+
+        System.out.println(cpuMove);
+        gameLogic(playerMove, cpuMove);
+
+    }
+
+    public static void movesPrinter(){
+        System.out.println("Please select your move: ");
+        //this prints off the moves one can take while playing rock/paper/scissors/lizard/spock
+        for (int i = 0; i < moves.size(); i++) {
+            System.out.println(moves.get(i));
+        }
+    }
+    public static void moveSetter(){
+        //this adds Strings to the ArrayList
+        moves.add("Rock");
+        moves.add("Paper");
+        moves.add("Scissors");
+        moves.add("Lizard");
+        moves.add("Spock");
+    }
+    
+    public static void gameLogic(String playerMove, String cpuMove){
+        if (playerMove.equals(cpuMove)) {
+            System.out.println("Tie!!");
+            System.out.println("No one wins because "+ playerMove + " is the same as " + cpuMove);
+        } else if (playerMove.equalsIgnoreCase("paper") && (cpuMove.equalsIgnoreCase("scissors") || cpuMove.equalsIgnoreCase("lizard"))) {
+            System.out.println("CPU Wins!!");
+            System.out.println("CPU wins because "+ playerMove + " loses to " + cpuMove);
+        }else if (playerMove.equalsIgnoreCase("scissors") && (cpuMove.equalsIgnoreCase("rock") || cpuMove.equalsIgnoreCase("spock"))) {
+            System.out.println("CPU Wins!!");
+            System.out.println("CPU wins because "+ playerMove + " loses to " + cpuMove);
+        }else if (playerMove.equalsIgnoreCase("lizard") && (cpuMove.equalsIgnoreCase("rock") || cpuMove.equalsIgnoreCase("scissors"))) {
+            System.out.println("CPU Wins!!");
+            System.out.println("CPU wins because "+ playerMove + " loses to " + cpuMove);
+        }else if (playerMove.equalsIgnoreCase("spock") && (cpuMove.equalsIgnoreCase("paper") || cpuMove.equalsIgnoreCase("lizard"))) {
+            System.out.println("CPU Wins!!");
+            System.out.println("CPU wins because "+ playerMove + " loses to " + cpuMove);
+        } else if (playerMove.equalsIgnoreCase("rock") && (cpuMove.equalsIgnoreCase("paper") || cpuMove.equalsIgnoreCase("spock"))) {
+            System.out.println("CPU Wins!!");
+            System.out.println("CPU wins because " + playerMove + " loses to " + cpuMove);
+        } else {
+            System.out.println("Player smoked CPU");
+            System.out.println("Player wins because "+ cpuMove + " loses to " + playerMove);
+        }
+    }
+
+}
---
+++ src/MainTest.java
@@ -0,0 +1,20 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @org.junit.jupiter.api.Test
+    void main() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void movesPrinter() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void moveSetter() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void gameLogic() {
+    }
+}
